08f8be6716989d0fbde077d7c40c452ab99f05f1
prasannavenkatesh.k <prasannavenkatesh.k@gofrugal.com>
2023-05-09 17:52:25 +0530

    Form Minor Cha nges

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent {
   title = 'SmartSave';
   welcome = 'Welcome Back';
   welcome_paragraph = 'Welcome Back, Please enter Your details';
+  description = 'Join the millions of smart investors who trust us to manage their finances. Login to access your personalized dashboard, track your portfolio performance, and make informed investment decisions.'
 
   signinForm = new FormGroup({
     userName: new FormControl(''),
